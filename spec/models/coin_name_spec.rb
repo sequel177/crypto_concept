b8f727b2726e84e19f4c4524204a5e89335aa644
India <india.rogers89@gmail.com>
2021-05-01 10:16:34 -0500

    Updated Spec File

--- spec/models/coin_name_spec.rb
+++ spec/models/coin_name_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe CoinName, type: :model do
-  it 'tests CoinName object' do
+  it 'tests coin object' do
   	#CoinName = CoinName.create({acronym: 'Sample acronym', price: 'Sample price'})
-  	coin_name = FactoryBot.create(:coin_name)
-  	expect(CoinName.acronym).to eq('Sample acronym')
+  	coin = FactoryBot.create(:coin_name)
+  	expect(coin.acronym).to eq('Sample acronym')
   end
 end
