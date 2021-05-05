e0f9519c6d6b8c0ecd199f23b32b1c20569f0861
India <india.rogers89@gmail.com>
2021-05-05 14:31:39 -0500

    Added seeds

--- spec/factories/coins.rb
+++ spec/factories/coins.rb
@@ -4,10 +4,4 @@ FactoryBot.define do
     ticker { "Sample ticker" }
     price { "Sample price" }
   end
-
-  factory :random_coin, class: Coin do
-  	name { Faker::CryptoCoin.coin_name }
-    ticker { Faker::CryptoCoin.acronym }
-    price { Faker::Commerce.price(range: 0..10.0, as_string: true) }
-  end
 end
